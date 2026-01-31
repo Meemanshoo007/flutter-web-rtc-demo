4f16a8416880736bb749e8cbe593ce403559ff83
Meemanshoo007 <meemanshoo@gmail.com>
2026-01-31 18:32:08 +0530

    resolving versal json file

--- lib/main.dart
+++ lib/main.dart
@@ -34,3 +34,24 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// {
+// "builds": [
+// {
+// "src": "build/web/**",
+// "use": "@vercel/static"
+// }
+// ],
+// "routes": [
+// { "src": "/(.*)", "dest": "/index.html" }
+// ],
+// "headers": [
+// {
+// "source": "/(.*)",
+// "headers": [
+// { "key": "Cross-Origin-Opener-Policy", "value": "same-origin" },
+// { "key": "Cross-Origin-Embedder-Policy", "value": "require-corp" }
+// ]
+// }
+// ]
+// }

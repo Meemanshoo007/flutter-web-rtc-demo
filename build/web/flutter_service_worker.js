379896d88b0db66c40f9f929ddd5767a2afe963f
Meemanshoo007 <meemanshoo@gmail.com>
2026-01-31 18:35:30 +0530

    resolving vercel json file try 2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3d73f14c8d662cae1ecc0bb5c10cccf6
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "33811f72f0d454a16cca2a2eae10a1e9",
+"flutter_bootstrap.js": "fccdccccb587e41a37145bb0d0abaafb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
